f13c9db40e33e8f50317566004837658c3368255
Drew Hayward <d.andrew.hayward@gmail.com>
2018-03-13 15:44:30 -0400

    Change to array dump

--- csvToJson.py
+++ csvToJson.py
@@ -3,9 +3,21 @@ import csv, json
 csvFile = open('Traffic_Violations.csv', 'r')
 jsonFile = open('Traffic_Violations.json', 'w')
 
-fields = ("Date Of Stop","Time Of Stop","Agency","SubAgency","Description","Location","Latitude","Longitude","Accident","Belts","Personal Injury","Property Damage","Fatal","Commercial License","HAZMAT","Commercial Vehicle","Alcohol","Work Zone","State","VehicleType","Year","Make","Model","Color","Violation Type","Charge","Article","Contributed To Accident","Race","Gender","Driver City","Driver State","DL State","Arrest Type","Geolocation")
+fields = ("Date Of Stop","Time Of Stop","Agency","SubAgency","Description",
+    "Location","Latitude","Longitude","Accident","Belts","Personal Injury",
+    "Property Damage","Fatal","Commercial License","HAZMAT","Commercial Vehicle",
+    "Alcohol","Work Zone","State","VehicleType","Year","Make","Model","Color",
+    "Violation Type","Charge","Article","Contributed To Accident","Race","Gender",
+    "Driver City","Driver State","DL State","Arrest Type","Geolocation")
 
 reader = csv.DictReader(csvFile, fields)
+next(reader, None)
+
+dictList = []
+
 for row in reader:
-    json.dump(row, jsonFile)
-    jsonFile.write('\n')
+    dictList.append(row)
+
+    #jsonFile.write('\n')
+
+json.dump(dictList, jsonFile, separators=(',', ':'))
