8b77539c1533d08db30ad14eb1780d38ac7f4356
Joe Holston <holstonjx1@gcc.edu>
2018-03-14 20:58:32 -0400

    Task3 functions updated.

--- task2.py
+++ task2.py
@@ -15,7 +15,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 # Connect to local DynamoDB instance
-dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8080')
 
 table = dynamodb.Table('violations')
 
--- task3.py
+++ task3.py
@@ -3,17 +3,34 @@ import os
 os.environ["TZ"] = "UTC"
 
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 # Connect to local DynamoDB instance
 dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8080')
 
 table = dynamodb.Table('violations')
 
+
 def findByDate(searchDate):
+    response = table.scan(
+        FilterExpression=Key('date_of_stop').eq(searchDate)
+    )
+    count = 0
+    for i in response['Items']:
+        count += 1
+    return count
+
 
 def countOutOfStateCars():
-    response = table.query(
-        KeyConditionExpression=Key('Driver State').ne('MD')
+    response = table.scan(
+        FilterExpression=Attr('driver_state').ne('MD')
     )
+    count = 0
+    for i in response['Items']:
+        count += 1
+    return count
 
-def findWorstOwners():
+#def findWorstOwners():
+
+print("Date Search:", findByDate('08/29/2017'))
+print("Out of state cars:", countOutOfStateCars())
